5c0ad6927b0df894e927fede0dcd9586c1f5aece
Haribo7891 <harry.crank@me.com>
2018-02-05 10:21:08 +0000

    Created IBOutlets and functionality to guess numbers

--- Five Finger Guess/ViewController.swift
+++ Five Finger Guess/ViewController.swift
@@ -10,6 +10,21 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var fingersTextField: UITextField!
+    
+    @IBAction func guess(_ sender: Any) {
+        
+        let diceRoll = String(arc4random_uniform(6))
+        
+        if fingersTextField.text == diceRoll {
+            resultLabel.text = "You're right! Well done."
+        } else {
+            resultLabel.text = "Wrong! It was a \(diceRoll)."
+        }
+    }
+    
+    @IBOutlet weak var resultLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
